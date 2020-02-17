ef51c414d0bd0ba20821dda21838036f57a7bcbc
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2020-02-17 21:33:14 +0000

    Improved randomness of spawns and prevented overlaps

--- MetalDetectingSim/Assets/Test Scripts/SpawnArtefacts.cs
+++ MetalDetectingSim/Assets/Test Scripts/SpawnArtefacts.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class SpawnArtefacts : MonoBehaviour
 {
-    public int num = 12;
+    public int width;
+    public int height;
     public GameObject cube;
+    [Range(0.0f, 100.0f)]
+    public float chance;
     public List<string> metal = new List<string>();
     public List<string> shape = new List<string>();
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < num; i++)
+        int area = width * height;
+        for (int i = 0; i < area ; i++)
         {
-            Debug.Log("HERE");
-            var newObject = (GameObject) Instantiate(cube, new Vector3(1, 0, 1+i*2), Quaternion.identity);
-            newObject.name = metal[Random.Range(0, metal.Count)] + ' ' + shape[Random.Range(0, shape.Count)];
+            float check = Random.Range(0f, 100f);
+            if (check < chance)
+            {
+                var newObject = (GameObject)Instantiate(cube, new Vector3(.5f+Mathf.Floor(i/width), 0, (.5f + i)-(Mathf.Floor(i/height)*height)), Quaternion.identity);
+                newObject.name = metal[Random.Range(0, metal.Count)] + ' ' + shape[Random.Range(0, shape.Count)];
+
+            }
         }
     }
 
