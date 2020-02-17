5704c99e7592ee1c3c3ebe1c049c4f012d2f670b
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2020-02-17 21:11:05 +0000

    Added 2 scripts
    first script detects collision of metal detector with artefact and prints a number based on material and shape
    
    Second script randomly spawns objects of varying metals and shapes

--- MetalDetectingSim/Assets/Test Scripts/DetectorReading.cs
+++ MetalDetectingSim/Assets/Test Scripts/DetectorReading.cs
@@ -13,6 +13,17 @@ public class DetectorReading : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (other.gameObject.tag == "Artefact")
+        {
+            if (other.gameObject.name.Contains("gold"))
+            {
+                Debug.Log("70");
+            }
+        }
     }
 }
---
+++ MetalDetectingSim/Assets/Test Scripts/SpawnArtefacts.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArtefacts : MonoBehaviour
+{
+    public int num = 12;
+    public GameObject cube;
+    public List<string> metal = new List<string>();
+    public List<string> shape = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < num; i++)
+        {
+            Debug.Log("HERE");
+            var newObject = (GameObject) Instantiate(cube, new Vector3(1, 0, 1+i*2), Quaternion.identity);
+            newObject.name = metal[Random.Range(0, metal.Count)] + ' ' + shape[Random.Range(0, shape.Count)];
+        }
+    }
+
+}
