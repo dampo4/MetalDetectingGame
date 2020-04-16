c9b6737b21f86e068c7cd4cd4232b02417a768e4
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2020-04-16 14:18:46 +0100

    Made interactable radius visible

--- MetalDetectingSim/Assets/Test Scripts/Interactable.cs
+++ MetalDetectingSim/Assets/Test Scripts/Interactable.cs
@@ -3,4 +3,9 @@
 public class Interactable : MonoBehaviour
 {
     public float radius = 3f;
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
 }
