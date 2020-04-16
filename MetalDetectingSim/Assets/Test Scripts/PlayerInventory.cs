6992a3813da11ed71bd15b9eb9ebb8e08f5c8594
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2020-04-16 15:58:00 +0100

    Created item blueprint script

--- MetalDetectingSim/Assets/Test Scripts/PlayerInventory.cs
+++ MetalDetectingSim/Assets/Test Scripts/PlayerInventory.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void UpdateUI()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
