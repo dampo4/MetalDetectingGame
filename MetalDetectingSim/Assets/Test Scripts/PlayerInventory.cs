cd8be438f489a1eed2d173e5b4ceabcd5cd7eeca
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2020-04-16 16:49:54 +0100

    Added reference to all slots in PlayerInventory script

--- MetalDetectingSim/Assets/Test Scripts/PlayerInventory.cs
+++ MetalDetectingSim/Assets/Test Scripts/PlayerInventory.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+    public Transform itemsParent;
+    InventorySlot[] slots
+
+    private void Start()
+    {
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+    }
     public void UpdateUI(Item item)
     {
         Debug.Log("Updating UI"+ item.name);
+
     }
 }
