850a66e9b9bb110abef8026cb379097d7907d837
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2020-05-26 16:51:03 +0100

    Made it possible to click on an item to see it close up

--- MetalDetectingSim/Assets/Test Scripts/ShowImage.cs
+++ MetalDetectingSim/Assets/Test Scripts/ShowImage.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowImage : MonoBehaviour
 {
+    private Image current;
+    public Image display;
     public void Display(int icon)
     {
         Debug.Log(icon);
-        
+        if (icon == 0)
+        {
+            current = GameObject.Find("/Canvas/Inventory/Panel/InventorySlot/ItemButton/Icon").GetComponent<Image>();
+        }
+        else
+        {
+            current = GameObject.Find("/Canvas/Inventory/Panel/InventorySlot(" + icon + ")/ItemButton/Icon").GetComponent<Image>();
+        }
+        Debug.Log(current.sprite.name);
+        display.sprite = current.sprite;
+        display.enabled = true;
     }
 }
