66b73f71d9552d29c08fc0df8395b5f77b609d76
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2020-05-26 22:48:04 +0100

    Added Fenced exterior and colliders

--- MetalDetectingSim/Assets/Test Scripts/ClampDetector.cs
+++ MetalDetectingSim/Assets/Test Scripts/ClampDetector.cs
@@ -8,12 +8,12 @@ public class ClampDetector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 namePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        DetectorReading.transform.position = namePos;
     }
 }
