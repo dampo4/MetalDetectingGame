7ace597a3545ecb7cb019ca6113f10e0c5faefb0
dampo4 <kieran.jolly@students.plymouth.ac.uk>
2020-05-26 16:25:05 +0100

    Added button script to all inventory icons

---
+++ MetalDetectingSim/Assets/Test Scripts/ShowImage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowImage : MonoBehaviour
+{
+    public void Display(int icon)
+    {
+        Debug.Log(icon);
+        
+    }
+}
